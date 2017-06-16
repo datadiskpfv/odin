d2dc3e78a7c47312fbc646168a6b46a01906bf80
Paul Valle <paul.valle@datadisk.co.uk>
2017-06-16 09:19:29 +0100

    finished subscript project

--- scratch/sc.rb
+++ scratch/sc.rb
@@ -1,28 +1,18 @@
-
 def substrings(words, dic = {})
-  #puts words
-  p dic
-  results = Hash.new(0)
+  results = Hash.new
+  words.downcase!
 
-  words.split(' ').each do |word|
-    dic.each do |w|
-      if word.downcase.include?(w) == true
-        #puts "Found: #{w}"
-        results[w] = results[w] + 1
-      end
-    end
+  dic.each do |w|
+    substring_occurrences = words.scan(/#{w}/).length
+    results[w.to_sym] = substring_occurrences unless substring_occurrences.zero?
   end
 
-  printf("#{results.sort}")
+  p results.sort
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 substrings("below", dictionary)
 # {"below"=>1, "low"=>1}
-
 substrings("below down", dictionary)
-puts
-
 substrings("Howdy partner, sit down! How's it going?", dictionary)
-
