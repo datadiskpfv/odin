bbede1defcde8aec8756c07938f639305c2ca3bb
Paul Valle <paul.valle@datadisk.co.uk>
2017-06-16 16:18:05 +0100

    finished first bubble project

--- bubble_sort/bs1.rb
+++ bubble_sort/bs1.rb
@@ -1,9 +1,9 @@
 def bubble_sort(a1)
   p a1
-  a1.length.times do |index|
+  a1.length.times do
     (a1.length-1).times do |index|
       while a1[index] > a1[index+1]
-          a1[index], a1[index+1] = a1[index+1], a1[index]
+       a1[index], a1[index+1] = a1[index+1], a1[index]
       end
     end
   end
