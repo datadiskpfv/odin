57f6a381c3c8e3ec5b14629e38d1e6826cb802ca
Paul Valle <paul.valle@datadisk.co.uk>
2017-06-16 08:48:48 +0100

    finished subscript project

--- scratch/sc.rb
+++ scratch/sc.rb
@@ -1,23 +1,25 @@
 
 def substrings(words, dic = {})
-  puts words
+  #puts words
   p dic
+  results = Hash.new(0)
 
   words.split(' ').each do |word|
     dic.each do |w|
-      if word.include?(w) == true
-        puts "Found: #{w}"
+      if word.downcase.include?(w) == true
+        #puts "Found: #{w}"
+        results[w] = results[w] + 1
       end
     end
   end
-end
 
+  printf("#{results.sort}")
+end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 substrings("below", dictionary)
 # {"below"=>1, "low"=>1}
-puts
 
 substrings("below down", dictionary)
 puts
