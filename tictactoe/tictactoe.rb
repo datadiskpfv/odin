dec29fe0ac0d7cd5f3d2fcc2cd77e472e33419d7
Paul Valle <paul.valle@datadisk.co.uk>
2017-06-20 13:49:36 +0100

    started tictactoe project

--- tictactoe/tictactoe.rb
+++ tictactoe/tictactoe.rb
@@ -1,25 +1,62 @@
 class Board
 
   def initialize
-    @grid = Array.new(3) { Array.new(3, 0) }
+    @grid = Array.new(3) { Array.new(3, " ") }
   end
 
   def display
-    p @grid
+
+    char = 65
+
+    puts "     1 |  2 |  3 "
+    @grid.each_with_index do |row, index|
+
+      puts "       |    |   "
+      puts "#{(char).chr}   #{row[0]}  | #{row[1]}  | #{row[2] }  "
+      puts "       |    |   "
+
+      char += 1
+
+      if index < 2
+        puts "    -------------"
+      end
+    end
   end
 
-  def add_move
+  def add_move(square, player)
+    puts "Making a move"
+
+    case (square[0].to_s)
+      when 'A'
+        index = 0;
+      when 'B'
+        index = 1;
+      when 'C'
+        index = 2
+    end
+
+    if ['X', 'O'].include? @grid[index][square[1].to_i - 1]
+      puts "That square #{square} has already been taken, try again"
+      return 1
+    else
+      @grid[index][square[1].to_i - 1] = player
+    end
+
+
   end
 
   def check_winner
   end
 end
 
-play = true
 winner = false
+player = 'O'
+turns = 1
 
-while (play == true)
+while true
   r1 = ''
+  square = ''
+  play = true
 
   ## clear the screen
   #puts "\e[H\e[2J"
@@ -37,5 +74,41 @@ while (play == true)
 
   board = Board.new
   board.display
+
+  while play
+    puts
+
+    bad_input = true
+    while bad_input
+      printf("Player #{player}, select a square A1-C3: ")
+      square = gets.chomp.to_s
+
+      if square =~ /(A|B|C)[1-3]/
+        bad_input = false
+      else
+        puts "Wrong data entered"
+      end
+    end
+
+    board.add_move(square, player)
+    board.display
+
+
+    ## if no one has won by this point its a draw
+    if turns == 9
+      puts "its a DRAW!!!!!"
+      play = false
+      turns = 0
+    else
+      turns += 1
+    end
+
+    ## switch the player
+    if player == 'X'
+      player = 'O'
+    else
+      player = 'X'
+    end
+  end
 
 end
